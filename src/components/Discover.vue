f632c15e58eeea9811821ca30c07972be9299be2
filippo-falcone <falconefilippo98@hotmail.com>
2024-05-13 17:58:01 +0200

    feat: add data to Discover.vue

--- src/components/Discover.vue
+++ src/components/Discover.vue
@@ -1,5 +1,79 @@
 <script>
-export default {}
+export default {
+    data(){
+        return {
+            data: [
+                {
+                    sectionTitle: 'Scopri i prodotti Made in Italy',
+                    info: [
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/ACS/Made_in_Italy/2024/Gateways/04_April/MII_IT_BUBBLE_Gifts_200x200._CB579229770_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/ACS/Made_in_Italy/2024/Gateways/04_April/MII_IT_BUBBLE_Food_200x200._CB579229770_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/ACS/Made_in_Italy/2024/Gateways/04_April/MII_IT_BUBBLE_Apparel_200x200._CB579229770_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/ACS/Made_in_Italy/2024/Gateways/04_April/MII_IT_BUBBLE_Home_200x200._CB579229770_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/ACS/Made_in_Italy/2024/Gateways/04_April/MII_IT_BUBBLE_Beauty_200x200._CB579229770_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/ACS/Made_in_Italy/2024/Gateways/04_April/MII_IT_BUBBLE_Handmade_200x200._CB579229770_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/ACS/Made_in_Italy/2024/Gateways/04_April/MII_IT_BUBBLE_Kitchen_200x200._CB579229770_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/ACS/Made_in_Italy/2024/Gateways/04_April/MII_IT_BUBBLE_Deco_200x200._CB579229770_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/ACS/Made_in_Italy/2024/Gateways/04_April/MII_IT_BUBBLE_Toys_200x200._CB579229770_.jpg',
+                        }
+                    ]
+                },
+                {
+                    sectionTitle: 'Scopri nuovi eccitanti prodotti',
+                    info: [
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/Brand_Owners_B2C/2024/GATEWAY/APRIL/BUBBLE/IT_Favoritos_LR._CB559119013_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/Brand_Owners_B2C/2024/GATEWAY/APRIL/BUBBLE/IT_Deals_Bubble_LR._CB559119013_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/Brand_Owners_B2C/2024/GATEWAY/APRIL/BUBBLE/IT_Electronics_LR._CB559113711_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/Brand_Owners_B2C/2024/GATEWAY/APRIL/BUBBLE/IT_Home_LR._CB559113711_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/Brand_Owners_B2C/2024/GATEWAY/APRIL/BUBBLE/IT_Kitchen_LR._CB559113711_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/Brand_Owners_B2C/2024/GATEWAY/APRIL/BUBBLE/IT_Fashion_HR._CB559113711_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/Brand_Owners_B2C/2024/GATEWAY/APRIL/BUBBLE/IT_Sports_LR._CB559113711_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/Brand_Owners_B2C/2024/GATEWAY/APRIL/BUBBLE/IT_Beauty_LR._CB559113711_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/Brand_Owners_B2C/2024/GATEWAY/APRIL/BUBBLE/IT_Food_LR._CB559113711_.jpg',
+                        },
+                        {
+                            image: 'https://images-eu.ssl-images-amazon.com/images/G/29/Brand_Owners_B2C/2024/GATEWAY/APRIL/BUBBLE/IT_ToysBaby_LR._CB559113711_.jpg',
+                        }
+                    ]
+                }
+            ]
+        };
+    }
+}
 </script>
 
 <template></template>
