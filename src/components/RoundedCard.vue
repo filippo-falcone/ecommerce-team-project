be8c65de5b5323d8168867f6b52aeca86a22334a
sofiacottone <cottonesofia@outlook.it>
2024-05-14 16:42:02 +0200

    feat: add rounded card component

---
+++ src/components/RoundedCard.vue
@@ -0,0 +1,12 @@
+<script></script>
+<template>
+    <div class="hstack">
+        <div class="vstack">
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/29/ACS/Made_in_Italy/2024/Gateways/04_April/MII_IT_BUBBLE_Gifts_200x200._CB579229770_.jpg" alt="">
+        </div>
+    </div>
+    
+</template>
+
+<style lang="scss">
+</style>
